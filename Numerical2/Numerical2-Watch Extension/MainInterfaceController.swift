254a43a6d6761c6661e6a9a8b5a1fc26ca4f013a
kevinenax <kevinenax@gmail.com>
2015-10-09 19:45:17 -0500

    Improve UI of main screen on 38mm watch face - Kevin E

--- Numerical2/Numerical2-Watch Extension/MainInterfaceController.swift
+++ Numerical2/Numerical2-Watch Extension/MainInterfaceController.swift
@@ -15,31 +15,22 @@ class MainInterfaceController: WKInterfaceController {
     @IBOutlet var mainLabel: WKInterfaceLabel!
     @IBOutlet var subLabel: WKInterfaceLabel!
     
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-    
+    @IBOutlet var topLabelGroup: WKInterfaceGroup!
+    @IBOutlet var buttonContainer: WKInterfaceGroup!
     
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        let device = WKInterfaceDevice.currentDevice()
+        if device.screenBounds.width < 156.0 {
+            topLabelGroup.setContentInset(UIEdgeInsetsMake(0, 8, 0, 5))
+            buttonContainer.setContentInset(UIEdgeInsetsMake(0, 0, 0, 0))
+        }
         let attrNumericalString = NSAttributedString(string: "Numerical", attributes: [NSFontAttributeName : UIFont.systemFontOfSize(24)])
         mainLabel.setAttributedText(attrNumericalString)
         subLabel.setText("A calculator with\nno equal")
     }
 
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-    
-    @IBAction func gridButtonPressed() {
-        
-    }
     
     @IBAction func speechButtonPressed() {
         presentTextInputControllerWithSuggestions(nil, allowedInputMode: .Plain) { (results) -> Void in
---
+++ Numerical2/Numerical2/Equation Store/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Numerical2
+//
+//  Created by Kevin Enax on 10/9/15.
+//  Copyright © 2015 Very Tiny Machines. All rights reserved.
+//
+
+import Foundation
