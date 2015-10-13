d53acbf8a9ae7237902b84996f1d72eca9c396d6
kevinenax <kevinenax@gmail.com>
2015-10-13 06:14:42 -0500

    Added color change on a timer to buttons in watch app - Kevin E

--- Numerical2/Numerical2-Watch Extension/WatchCalculatorOneButtonRow.swift
+++ Numerical2/Numerical2-Watch Extension/WatchCalculatorOneButtonRow.swift
@@ -13,18 +13,25 @@ class WatchCalculatorOneButtonRow: NSObject {
     @IBOutlet var button: WKInterfaceButton!
     var buttonTitle : String?
 
+    let defaultColor = UIColor(red:0.2, green:0.2, blue:0.2, alpha:1)
+    let highlightedColor = UIColor(red:0.4, green:0.4, blue:0.4, alpha:1)
     
     var delegate : WatchButtonDelegate?
     
     func configureButtons(leftString:String) {
         self.buttonTitle = leftString
         button.setTitle(leftString)
-    
+        button.setBackgroundColor(defaultColor)
     }
     
     @IBAction func leftButtonPressed() {
         if let delegate = self.delegate, leftString = self.buttonTitle {
             delegate.buttonPressedWithTitle(leftString);
+        }
+        button.setBackgroundColor(highlightedColor)
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) { () -> Void in
+            self.button.setBackgroundColor(self.defaultColor)
         }
     }
 
--- Numerical2/Numerical2-Watch Extension/WatchCalculatorThreeButtonRow.swift
+++ Numerical2/Numerical2-Watch Extension/WatchCalculatorThreeButtonRow.swift
@@ -17,22 +17,33 @@ class WatchCalculatorThreeButtonRow: NSObject {
     @IBOutlet var rightButton: WKInterfaceButton!
     var rightString : String?
     
+    let defaultColor = UIColor(red:0.2, green:0.2, blue:0.2, alpha:1)
+    let highlightedColor = UIColor(red:0.4, green:0.4, blue:0.4, alpha:1)
+    
     var delegate : WatchButtonDelegate?
     
     func configureButtons(leftString:String, middleString:String, rightString:String) {
         self.leftString = leftString
         leftButton.setTitle(leftString)
+        leftButton.setBackgroundColor(defaultColor)
         
         self.middleString = middleString
         middleButton.setTitle(middleString)
+        middleButton.setBackgroundColor(defaultColor)
         
         self.rightString = rightString
         rightButton.setTitle(rightString)
+        rightButton.setBackgroundColor(defaultColor)
     }
     
     @IBAction func leftButtonPressed() {
         if let delegate = self.delegate, leftString = self.leftString {
             delegate.buttonPressedWithTitle(leftString);
+        }
+        leftButton.setBackgroundColor(highlightedColor)
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) { () -> Void in
+            self.leftButton.setBackgroundColor(self.defaultColor)
         }
     }
     
@@ -40,11 +51,21 @@ class WatchCalculatorThreeButtonRow: NSObject {
         if let delegate = self.delegate, middleString = self.middleString {
             delegate.buttonPressedWithTitle(middleString);
         }
+        middleButton.setBackgroundColor(highlightedColor)
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) { () -> Void in
+            self.middleButton.setBackgroundColor(self.defaultColor)
+        }
     }
     
     @IBAction func rightButtonPressed() {
         if let delegate = self.delegate, rightString = self.rightString {
             delegate.buttonPressedWithTitle(rightString);
+        }
+        rightButton.setBackgroundColor(highlightedColor)
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(0.1 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) { () -> Void in
+            self.rightButton.setBackgroundColor(self.defaultColor)
         }
     }
 }
